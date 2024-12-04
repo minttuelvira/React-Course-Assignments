e7762e1430efacdcf94393db42a254b1ba4f6d1e
minttuelvira <minttuelvira@hotmail.com>
2024-12-04 13:38:56 +0200

    Added an array of the products

---
+++ src/ComponentAssignment/ProductSelector.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const products = [
+    { name: 'AMD Ryzen 5 7600', price: 200 },
+    { name: 'AMD Ryzen 9 9950X', price: 690 },
+    { name: 'AMD Ryzen 5 3600', price: 85 },
+  ];
+
+
+  
