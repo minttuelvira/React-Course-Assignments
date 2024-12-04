9f674a7b61b16f52c3db1df67c6a683345fd4752
minttuelvira <minttuelvira@hotmail.com>
2024-12-04 14:38:57 +0200

    Added header + App is now ready (CSS needs to be added still)

--- src/ComponentAssignment/ComponentUI.jsx
+++ src/ComponentAssignment/ComponentUI.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import ProductSelector from './ProductSelector';
 import OrderInfo from './OrderInfo';
 import './ComponentUI.css';
+import logo from './pc-photo.png';
 
 const App = () => {
-    
+
     const [selectedProduct, setSelectedProduct] = useState({ name: 'AMD Ryzen 5 7600', price: 200 });
     const [quantity, setQuantity] = useState(1);
 
@@ -17,5 +18,29 @@ const App = () => {
           setQuantity(newQuantity);
         }
       };
+
+      return (
+        
+        <div className="app-container">
+
+          <header>
+            <img src={logo} alt="Logo" className="logo" />
+            <h1>Welcome to product page!</h1>
+          </header>
+
+          <main>
+            <ProductSelector
+              selectedProduct={selectedProduct}
+              quantity={quantity}
+              onProductChange={handleProductChange}
+              onQuantityChange={handleQuantityChange}
+            />
+            <OrderInfo selectedProduct={selectedProduct} quantity={quantity} />
+          </main>
+        </div>
+      );
+      
       
 }
+
+export default App;
