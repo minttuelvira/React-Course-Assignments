d46504423349ec7918bcf2e993ca9a747b5de836
minttuelvira <minttuelvira@hotmail.com>
2024-12-04 14:23:42 +0200

    Product selector and changes in values

--- src/ComponentAssignment/ComponentUI.jsx
+++ src/ComponentAssignment/ComponentUI.jsx
@@ -2,3 +2,20 @@ import React, { useState } from 'react';
 import ProductSelector from './ProductSelector';
 import OrderInfo from './OrderInfo';
 import './ComponentUI.css';
+
+const App = () => {
+    
+    const [selectedProduct, setSelectedProduct] = useState({ name: 'AMD Ryzen 5 7600', price: 200 });
+    const [quantity, setQuantity] = useState(1);
+
+    const handleProductChange = (product) => {
+        setSelectedProduct(product);
+      };
+      
+      const handleQuantityChange = (newQuantity) => {
+        if (newQuantity >= 0) {
+          setQuantity(newQuantity);
+        }
+      };
+      
+}
