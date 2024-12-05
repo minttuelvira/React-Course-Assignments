c9df729bd8733076facc81845dd5a218850b74ea
minttuelvira <minttuelvira@hotmail.com>
2024-12-05 10:53:58 +0200

    Added CSS and a few classes

--- src/OpenData/WeatherUI.jsx
+++ src/OpenData/WeatherUI.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import './Weather.css';
 
 const WeatherApp = () => {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState(null);
+
 
   const apiKey = '';
 
@@ -13,11 +15,11 @@ const WeatherApp = () => {
   }, [city]);
 
   return (
-    <div>
+    <div class="weather-container">
       <h1>Weather App</h1>
       <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city"/>
       {weather && weather.main && (
-        <div>
+        <div class="weather-info">
           <h2>{weather.name}</h2>
           <p>{weather.weather[0].description}</p>
           <p>Temperature: {weather.main.temp} °C</p>
